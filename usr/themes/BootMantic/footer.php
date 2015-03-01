e9110e1a6ee3ed951cc525b8c1a3a543d0d335e5
joe chen <gdjustwuxia@gmail.com>
2015-03-01 21:28:23 +0800

    修改footer

--- usr/themes/BootMantic/footer.php
+++ usr/themes/BootMantic/footer.php
@@ -5,10 +5,8 @@
     <div class="container">
         <p>
             &copy; <?php echo date('Y'); ?> <a href="<?php $this->options->siteUrl(); ?>"><?php $this->options->title(); ?></a>.
-            <?php _e('Theme By <a href="http://blog.sloger.info" target="_blank">Sloger</a>. '); ?>
             <?php _e('由 <a href="http://www.typecho.org" target="_blank">Typecho</a> 强力驱动'); ?>
         </p>
-        <p>部分代码和设计风格参考<a href="http://www.dandyweng.com" target="_blank">Dandy Weng</a> 和 <a href="http://blog.dandyweng.com" target="_blank">Dandy's Blog</a></p>
     </div>
 </footer>
 
